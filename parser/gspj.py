13c435c85c0be41452a3c0b898a34a1a7b9bfc3a
I-hsin-Chen <ihsin105521@gmail.com>
2024-05-22 20:47:16 +0800

    Update gspj method

--- parser/gspj.py
+++ parser/gspj.py
@@ -18,7 +18,7 @@ def extract_tables_and_conditions(query):
     '''
     # extract table names
     tables = re.findall(r'FROM\s+(\w+)\s+(\w+)', query, re.IGNORECASE)
-    tables += re.findall(r',\s+(\w+)\s+(\w+)', query, re.IGNORECASE)
+    tables += re.findall(r',\s*(\w+)\s+(\w+)', query, re.IGNORECASE)
     
     # extract conditions
     where_clause = re.search(r'WHERE(.*?);', query, re.DOTALL | re.IGNORECASE)
@@ -60,23 +60,28 @@ def greedy_selective_pairwise_join(query):
     This function performs a greedy selective pairwise join (GSPJ) on the given query.
     The returned query is the final query after performing GSPJ.
     '''
+    
+    # Extract all the tables and conditions from the query
     tables, conditions = extract_tables_and_conditions(query)
     
     while True:
         cardinalities = {}
         node_pairs = {}
+        
+        # Loop over all pairs of tables and extract conditions related to them
         for table1, table2 in list(itertools.combinations(tables, 2)):
             other_tables = [table for table in tables if table not in (table1, table2)]
             node_pairs[(table1, table2)] = extract_related_conditions(conditions, table1, table2, other_tables)
+            print(f'Conditions related to {table1} and {table2}: {node_pairs[(table1, table2)]}')
     
+        # Estimating cardinality for each table pair (node pair)
         for node_pair in node_pairs:
             table1, table2 = node_pair
             condition = ' AND '.join(node_pairs[node_pair])
             query = f'SELECT COUNT(*) FROM {table1[0]} {table1[1]}, {table2[0]} {table2[1]} WHERE {condition};'
-            # print(query)
             
             import random
-            # for testing purposes, generate random cardinalities
+            # for testing purposes, generate random cardinalities temporarily
             cardinalities[(table1, table2)] = random.randint(0, 100)
         
         best_combination = min(cardinalities, key=cardinalities.get)
@@ -93,16 +98,22 @@ def greedy_selective_pairwise_join(query):
         tables.remove(table2)
         tables.append(new_table)
         
-        for condition in conditions:
-            if condition in node_pairs[best_combination]:
-                conditions.remove(condition)
-            elif table1[1] in condition:
-                condition = condition.replace(f"{table1[1]}.", f"{combined_alias}.")
-            elif table2[1] in condition:
-                condition = condition.replace(f"{table2[1]}.", f"{combined_alias}.")
-
+        # Remove the conditions that are already used in the best combination
+        # Update the table alias in the remaining conditions
+        conditions = [c for c in conditions if c not in node_pairs[best_combination]]
+        for i, condition in enumerate(conditions):
+            match_t1 = re.search(r'{}\.'.format(table1[1]), condition, re.IGNORECASE)
+            match_t2 = re.search(r'{}\.'.format(table2[1]), condition, re.IGNORECASE)
+            if match_t1:
+                conditions[i] = re.sub(r'{}\.(\w+)'.format(table1[1]), f'{combined_alias}.\\1', conditions[i])
+            if match_t2:
+                conditions[i] = re.sub(r'{}\.(\w+)'.format(table2[1]), f'{combined_alias}.\\1', conditions[i])
+        
+        # If there are only two tables left, then just simply join them and return the final query
         if len(tables) == 2:
-            final_query = f'SELECT COUNT(*) FROM {tables[0][0]} {tables[0][1]}, {tables[1][0]} {tables[1][1]} WHERE {condition};'
+            remaining_conditions = ' AND '.join(conditions)
+            final_query = f'SELECT COUNT(*) FROM {tables[0][0]} {tables[0][1]}, {tables[1][0]} {tables[1][1]} WHERE {remaining_conditions};' \
+                if remaining_conditions else f'SELECT COUNT(*) FROM {tables[0][0]} {tables[0][1]}, {tables[1][0]} {tables[1][1]};'
             print(final_query)
             break
     
@@ -112,10 +123,8 @@ def greedy_selective_pairwise_join(query):
 if __name__ == '__main__':
     query = """
 SELECT COUNT(*) 
-FROM movie_companies mc, title t, movie_info_idx mi_idx 
-WHERE t.id = mc.movie_id 
-AND t.id = mi_idx.movie_id
-AND mi_idx.info_type_id = 112
-AND mc.company_type_id = 2;
+FROM 
+movie_companies mc,title t,movie_keyword mk 
+WHERE t.id=mc.movie_id AND t.id=mk.movie_id AND mk.keyword_id=117;
 """
     greedy_selective_pairwise_join(query)
