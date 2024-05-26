293e99caff7a598402169d95b250cafee949277f
shinee22301617 <shinee22301617@gmail.com>
2024-05-26 20:56:29 +0800

    Improve run_with_sqlite structure

--- parser/run_with_sqlite.py
+++ parser/run_with_sqlite.py
@@ -3,31 +3,34 @@ import time
 from gspj import *
 from utils import *
 
+def execute_a_query(query):
+    """
+    Input: A Query
+    Output: Result of the query and the time taken to execute the query
+    """
+    conn = sqlite3.connect('../imdb.db')
+    c = conn.cursor()
+
+    start_time = time.time()
+    c.execute(query)
+    count = c.fetchone()[0]
+    conn.commit()
+    end_time = time.time()
+    conn.close()
+
+    return count, end_time - start_time
+
 
 if __name__ == '__main__':
     query = """
     SELECT COUNT(*) FROM movie_keyword mk,title t,cast_info ci WHERE t.id=mk.movie_id AND t.id=ci.movie_id AND t.production_year>2010 AND mk.keyword_id=8200;
     """
     
-    conn = sqlite3.connect('../imdb.db')
-    c = conn.cursor()
-    start_time = time.time()
+    original_result, original_time = execute_a_query(query)
+    print(original_result, original_time)
 
-    c.execute(query)
-    count = c.fetchone()[0]
-    print("Original Query's output:", count)
-    conn.commit()
-    end_time = time.time()
-    print(f"Original Query's execution time: {end_time - start_time()}s")
-
-    start_time = time.time()
     gspj_query = greedy_selective_pairwise_join(query)
-    print("With GSPJ algorithm, the query:", gspj_query)
-    c.execute(gspj_query)
-    count = c.fetchone()[0]
-    print(f"GSPJ Query's execution time: {end_time - start_time()}s")
-    conn.commit()
-    end_time = time.time()
-    print(f"GSPJ Query's execution time: {end_time - start_time()}s")
+    gspj_query_result, gspj_query_time = execute_a_query(gspj_query)
+    print(gspj_query_result, gspj_query_time)
 
-    conn.close()
+    
