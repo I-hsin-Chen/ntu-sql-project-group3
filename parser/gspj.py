308d22c6c79e3b6671fedabe1e271c1b1dafbc52
I-hsin-Chen <ihsin105521@gmail.com>
2024-05-27 18:56:14 +0800

    gspj bug fix

--- parser/gspj.py
+++ parser/gspj.py
@@ -30,7 +30,6 @@ def greedy_selective_pairwise_join(query):
         for table1, table2 in list(itertools.combinations(tables, 2)):
             other_tables = [table for table in tables if table not in (table1, table2)]
             node_pairs[(table1, table2)] = extract_related_conditions(conditions, table1, table2, other_tables)
-            # print(f'Conditions related to {table1} and {table2}: {node_pairs[(table1, table2)]}')
         
         # Estimating cardinality for each table pair (node pair)
         for node_pair in node_pairs:
@@ -39,14 +38,14 @@ def greedy_selective_pairwise_join(query):
             query = f'SELECT COUNT(*) FROM {table1[0]} {table1[1]}, {table2[0]} {table2[1]} WHERE {condition};'
             cardinalities[(table1, table2)] = cardinality_estimation(query) if query.upper().count("SELECT") == 1 \
                 else cardinality_estimation(flatten(query, original_query))
+            # print(f'Cardinality of {table1} and {table2} is {cardinalities[(table1, table2)]} with related conditions {node_pairs[(table1, table2)]}')
         
         best_combination = min(cardinalities, key=cardinalities.get)
-        # print(f'Best join is between {best_combination[0]} and {best_combination[1]} with cardinality {cardinalities[best_combination]}')
         
         table1, table2 = best_combination
-        if node_pairs[best_combination] == []:
-            raise Exception(f'Parsing error : No conditions found for joining {table1} and {table2}')
-        match_condition = ' AND '.join(node_pairs[best_combination])
+        # if node_pairs[best_combination] == []:
+        #     raise Exception(f'Parsing error : No conditions found for joining {table1} and {table2}')
+        match_condition = '\nWHERE ' + ' AND '.join(node_pairs[best_combination]) if node_pairs[best_combination] else ''
         combined_alias = f"{table1[1]}_{table2[1]}"
         
         # Remove the conditions that are already used in the best combination
@@ -70,9 +69,9 @@ def greedy_selective_pairwise_join(query):
                         else re.sub(r'{}\.(\w+)'.format(t[1]), f'{combined_alias}.\\1', conditions[i])
                     
         selected_columns = ','.join(remove_duplicates(selected_columns)) if selected_columns else "*"
-        subquery = f'(SELECT {selected_columns} FROM {table2[0]} {table2[1]},{table1[0]} {table1[1]}\nWHERE {match_condition})' \
+        subquery = f'(SELECT {selected_columns} FROM {table2[0]} {table2[1]},{table1[0]} {table1[1]}{match_condition})' \
             if IS_LEFT_JOIN \
-            else f'(SELECT {selected_columns} FROM {table1[0]} {table1[1]},{table2[0]} {table2[1]} WHERE {match_condition})'
+            else f'(SELECT {selected_columns} FROM {table1[0]} {table1[1]},{table2[0]} {table2[1]}{match_condition})'
         
         new_table = (subquery, combined_alias)
         tables.remove(table1)
@@ -83,8 +82,8 @@ def greedy_selective_pairwise_join(query):
         if len(tables) == 2:
             remaining_conditions = ' AND '.join(conditions)
             if remaining_conditions :
-                final_query = f'SELECT COUNT(*) \nFROM {tables[1][0]} {tables[1][1]},{tables[0][0]} {tables[0][1]} \nWHERE \n{remaining_conditions};'\
-                    if IS_LEFT_JOIN else f'SELECT COUNT(*) \nFROM {tables[0][0]} {tables[0][1]},{tables[1][0]} {tables[1][1]} \nWHERE \n{remaining_conditions};'
+                final_query = f'SELECT COUNT(*) \nFROM {tables[1][0]} {tables[1][1]},{tables[0][0]} {tables[0][1]} \nWHERE {remaining_conditions};'\
+                    if IS_LEFT_JOIN else f'SELECT COUNT(*) \nFROM {tables[0][0]} {tables[0][1]},{tables[1][0]} {tables[1][1]} \nWHERE {remaining_conditions};'
             else :
                 raise Exception(f'Parsing error : No conditions found for joining tables in the final query') 
             
@@ -94,7 +93,7 @@ def greedy_selective_pairwise_join(query):
 
 if __name__ == '__main__':
     query = """
-SELECT COUNT(*) FROM title t,cast_info ci,movie_info_idx mi_idx WHERE t.id=ci.movie_id AND t.id=mi_idx.movie_id AND t.kind_id=1 AND t.production_year<1959;
+SELECT COUNT(*) FROM cast_info ci,title t,movie_info mi WHERE t.id=mi.movie_id AND t.id=ci.movie_id;
 """
     print("The original query is :" + to_cross_join(query))
     gspj_query = greedy_selective_pairwise_join(query)
