043adb7f155e9f3735e6d3070fc6dbeabeb351e9
Hao Chen Jiang <67539990+b29201746@users.noreply.github.com>
2024-05-29 01:00:57 +0800

    updated visualize.py and my_parser.py
    visualize可以在original query跟generated query上
    my_parser.py 是用來取代原本的parser.py

---
+++ parser/my_parser.py
@@ -0,0 +1,139 @@
+import sqlparse
+# 取得FROM(包括)之前的部分
+def Fetch_base_stmt(stmt):
+    parsed = sqlparse.parse(stmt)
+    for stmt in parsed:
+        for token in stmt.tokens:
+            if token.ttype is sqlparse.tokens.Keyword and token.value.upper() == "FROM":
+                return str(stmt)[: str(stmt).upper().rfind("FROM")].strip() + " FROM "
+    return None
+
+
+def Fetch_Projection(stmt):
+    stmt_lower = stmt.lower()
+    start = stmt_lower.find("select") + len("select")
+    end = stmt_lower.find("from")
+    if start == -1 or end == -1:
+        return None
+    substring = stmt[start:end].strip()
+    substring = substring.split(',')
+    
+    return substring
+
+
+# 獲得所有identifiers
+def Fetch_identifiers(stmt):
+    parsed = sqlparse.parse(stmt)
+    tokens = parsed[0].tokens
+    identifiers = []
+    for token in tokens:
+        if isinstance(token, sqlparse.sql.Identifier):
+            identifiers.append(token)
+    identifiers = [token for token in tokens if isinstance(token, (sqlparse.sql.Identifier, sqlparse.sql.IdentifierList))]
+    identifiers_values = []
+    for identifier in identifiers:
+        print(identifier)
+        if isinstance(identifier, sqlparse.sql.IdentifierList):
+            for iden in identifier:
+                if(isinstance(iden, sqlparse.sql.Identifier)):
+                    identifiers_values.append(iden)
+        elif isinstance(identifier, sqlparse.sql.Identifier):
+            identifiers_values.append(identifier)
+    aliases = []
+    real_name = []
+
+    for identifier in identifiers_values:
+        identifier = str(identifier)
+        if " " in identifier:
+            aliases.append(identifier[identifier.rfind(' ')+1:].strip())
+            real_name.append(identifier[:identifier.rfind(' ')].strip())
+        else:
+            aliases.append(identifier)
+            real_name.append(identifier)
+    # exit()
+    return real_name, aliases
+
+
+# print(id_list)
+
+
+# 獲得所有conditions
+def Fetch_comparison(stmt):
+    parsed = sqlparse.parse(stmt)
+    conditions = [
+        token for token in parsed[0].tokens if isinstance(token, sqlparse.sql.Where)
+    ]
+    comparisons = []
+    for condition in conditions:
+        comparisons.extend(
+            [
+                str(token)
+                for token in condition.tokens
+                if isinstance(token, sqlparse.sql.Comparison)
+            ]
+        )
+
+    return comparisons
+
+
+
+
+
+# 將query拆成多個queries
+def split_query(stmt):
+    base = Fetch_base_stmt(stmt)
+    id_list = Fetch_identifiers(stmt)
+    cmp = Fetch_comparison(stmt)
+
+    id = []
+    for i in id_list:
+        if " " in i:
+            id.append(i.split(" ")[-1])
+    queries = []
+    for c in cmp:
+        query = base
+        for idx, i in enumerate(id):
+            if i in c:
+                query += id_list[idx] + ", "
+        if query.endswith(", "):
+            query = query[:-2]
+        query += " WHERE " + c + ";"
+        queries.append(query)
+
+    return queries
+
+
+# 取得WHERE(包括)之前的部分
+def get_part_before_where(stmt):
+    parsed = sqlparse.parse(stmt)
+    parsed_statement = parsed[0]
+    pre_where_clause = ""
+    for token in parsed_statement.tokens:
+        if "WHERE" in token.value.upper():
+            break
+        pre_where_clause += str(token)
+
+    return pre_where_clause.strip()
+
+
+# 重新排序 WHERE 條件
+def comparison_reorder(stmt, new_order):
+    conditions = []
+    parsed = sqlparse.parse(stmt)
+    for token in parsed[0].tokens:
+        if isinstance(token, sqlparse.sql.Where):
+            for subtoken in token.tokens:
+                if isinstance(subtoken, sqlparse.sql.Comparison):
+                    conditions.append(str(subtoken).strip())
+    conditions = [conditions[i] for i in new_order]
+    before_where = get_part_before_where(stmt)
+    reordered_sql_query = before_where + " WHERE " + " AND ".join(conditions) + ";"
+    return reordered_sql_query
+
+if __name__ == '__main__':
+
+    
+    query = "SELECT COUNT(*) FROM cast_info ci CROSS JOIN (SELECT t.id t_id FROM title t CROSS JOIN movie_info_idx mi_idx WHERE t.id=mi_idx.movie_id AND t.kind_id=1 AND t.production_year<1959) t_mi_idx WHERE t_mi_idx.t_id=ci.movie_id;"
+    name, alias = Fetch_identifiers(query)
+    print(name)
+    print(alias)
--- parser/visualize.py
+++ parser/visualize.py
@@ -1,9 +1,9 @@
 import sqlparse
-from parse import Fetch_comparison
-from sqlparse.sql import Identifier, IdentifierList, Comparison, Where, Function
+from my_parser import *
+from sqlparse.sql import Identifier, IdentifierList, Function
 from sqlparse.tokens import Keyword
-
-
+import re
+from graphviz import Digraph
 class TreeNode:
     def __init__(self, value):
         self.value = value
@@ -13,57 +13,26 @@ class TreeNode:
     def add_child(self, child_node):
         self.children.append(child_node)
 
-
 def parse_sql(stmt):
+    if stmt.strip()[0] == '(' and stmt.strip()[-1] == ')':
+        stmt = stmt.strip()[1:-1]
     parsed = sqlparse.parse(stmt)[0]
-    tables = []
+    tables, aliases = Fetch_identifiers(stmt)
     conditions = Fetch_comparison(stmt)
-    projections = []
-    aliases = {}
-    from_seen = False
-    select_seen = False
-
-    for token in parsed.tokens:
-        if token.ttype is Keyword.DML and "SELECT" in token.value.upper():
-            select_seen = True
-        elif token.ttype is Keyword and token.value.upper() == "FROM":
-            from_seen = True
-            select_seen = False
-        elif token.ttype is Keyword and "WHERE" in token.value.upper():
-            from_seen = False
-        elif from_seen and isinstance(token, IdentifierList):
-            for identifier in token.get_identifiers():
-                tables.append(identifier.get_real_name())
-                if identifier.has_alias():
-                    aliases[identifier.get_real_name()] = identifier.get_alias()
-        elif from_seen and isinstance(token, Identifier):
-            tables.append(token.get_real_name())
-        elif select_seen and isinstance(token, IdentifierList):
-            for identifier in token.get_identifiers():
-                projections.append(str(identifier))
-        elif select_seen and isinstance(token, Function):
-            projections.append(str(token))
-        elif select_seen and isinstance(token, Identifier):
-            print("identifier:", identifier)
-            projections.append(str(token))
-    if projections == []:
-        start = stmt.lower().find("select") + len("select")
-        end = stmt.lower().find("from")
-        substring = stmt[start:end].strip()
-        projections.append(substring)
+    projections = Fetch_Projection(stmt)
+    aliases = dict(zip(tables, aliases))
+    #print 所有資訊
+    print("=====================================================================")
     print("Tables:", tables)
     print("Aliases:", aliases)
     print("Conditions:", conditions)
     print("Projections:", projections)
-    return tables, aliases, conditions, projections
-
-
-import re
+    print("=====================================================================")
+    return aliases, conditions, projections
 
 pattern = r"(=|>|<|' ')"
 
-
-def build_tree(tables, aliases, conditions, projections):
+def build_tree(aliases, conditions, projections):
     last_node = None
     for condition in conditions:
         select_conditions = ""
@@ -78,32 +47,27 @@ def build_tree(tables, aliases, conditions, projections):
             join_conditions = condition
 
         if select_conditions:
-            # print("select = ", select_conditions)
             root = TreeNode(f"σ {select_conditions}")
-
             if last_node is not None:
                 root.current_table = last_node.current_table
             for key, alias in aliases.items():
-                if alias in condition:
+                if alias+'.' in condition:
                     if alias not in root.current_table:
                         root.current_table.append(alias)
                         new_node = TreeNode(alias)
                         new_node.current_table.append(alias)
-
                         root.add_child(new_node)
 
         if join_conditions:
             root = TreeNode(f"⨝ {join_conditions}")
-
             if last_node is not None:
                 root.current_table = last_node.current_table
             for key, alias in aliases.items():
-                if alias in condition:
+                if alias+'.' in condition:
                     if alias not in root.current_table:
                         root.current_table.append(alias)
                         new_node = TreeNode(alias)
                         new_node.current_table.append(alias)
-
                         root.add_child(new_node)
 
         if last_node is not None:
@@ -114,23 +78,61 @@ def build_tree(tables, aliases, conditions, projections):
     root.add_child(last_node)
     return root
 
-
 def print_tree(node, level=0, prefix=""):
     if node is not None:
         print(" " * (level * 4) + prefix + str(node.value))
         for child in node.children:
             print_tree(child, level + 1, "└── ")
+            
+def traverse_tree(node, aliases):
+    
+    if node is not None:
+        new_node = node  
+        for key, alias in aliases.items():
+            if "CROSS" not in key and "SELECT" not in key:
+                continue
+            if new_node.value == alias:
+                print("KEY = ", key)
+                a, c, p = parse_sql(key)
+                new_node.add_child(build_tree(a, c, p))  
+                traverse_tree(new_node, a)
+        if 'σ' in new_node.value:
+            for child in new_node.children:
+                if 'σ' in child.value:
+                    new_node.children = child.children
+                    new_node.value = new_node.value + " AND" + child.value[1:]
 
+        for i in range(len(new_node.children)):
+            new_node.children[i] = traverse_tree(new_node.children[i], aliases)
+        
+        return new_node
 
-# # stmt = "SELECT P.Pnumber, P.Dnum, E.Lname, E.Address, E.Bdate FROM PROJECT P, DEPARTMENT D, EMPLOYEE E WHERE P.Dnum=D.Dnumber AND D.Mgr_ssn=E.Ssn AND P.Plocation= 'Stafford';"
-stmt = "SELECT COUNT(*) FROM movie_companies mc,title t,movie_info_idx mi_idx WHERE t.id=mc.movie_id AND t.id=mi_idx.movie_id AND mi_idx.info_type_id=112 AND mc.company_type_id=2;"
-# Parse the SQL statement
-tables, aliases, conditions, projections = parse_sql(stmt)
+def visualize_tree(node):
+    dot = Digraph()
+    dot.attr('node', fontname="Segoe UI Symbol")  # 指定使用 Segoe UI Symbol 字型
 
-# Build the query tree
-root = build_tree(tables, aliases, conditions, projections)
-print("=====================================================================")
-print("Original query:\n", stmt)
-print("=====================================================================")
-# Visualize the query tree
-print_tree(root)
+    def add_nodes_edges(node, dot):
+        label = node.value
+        dot.node(str(id(node)), label=label)
+        for child in node.children:
+            dot.edge(str(id(node)), str(id(child)))
+            add_nodes_edges(child, dot) 
+
+    add_nodes_edges(node, dot)
+    return dot
+
+if __name__ == '__main__':
+    # 換成要測試的query
+    stmt = "SELECT COUNT(*) FROM (SELECT mk.movie_id mk_movie_id,mi.movie_id mi_movie_id FROM movie_keyword mk CROSS JOIN movie_info mi WHERE mk.keyword_id>1633) mi_mk CROSS JOIN (SELECT mi_idx_t.t_id mi_idx_t_t_id FROM (SELECT t.id t_id FROM title t CROSS JOIN movie_info_idx mi_idx WHERE t.id=mi_idx.movie_id AND t.kind_id>1) mi_idx_t CROSS JOIN movie_companies mc WHERE mi_idx_t.t_id=mc.movie_id) mc_mi_idx_t WHERE mc_mi_idx_t.mi_idx_t_t_id=mi_mk.mk_movie_id AND mc_mi_idx_t.mi_idx_t_t_id=mi_mk.mi_movie_id;"
+    aliases, conditions, projections = parse_sql(stmt)
+
+    root = build_tree(aliases, conditions, projections)
+    traverse_tree(root, aliases)
+    
+    print("=====================================================================")
+    print("Original query:\n", stmt)
+    print("=====================================================================")
+    print_tree(root)
+    dot = visualize_tree(root)
+    dot.render('tree', format='png', view=True)
+    
