0892c537bd68147d731b209165ac13a1640766a5
I-hsin-Chen <ihsin105521@gmail.com>
2024-05-28 23:22:35 +0800

    by pass timeout and error

--- parser/run_with_sqlite.py
+++ parser/run_with_sqlite.py
@@ -51,11 +51,12 @@ def execute_a_query(query, timeout=500):
         if isinstance(result, str) and result.startswith('Error'):
             print(result)
             return result, 100000
-        print('Query {} executed successfully'.format(query))
 
     
     end_time = time.time()
     runtime = round(end_time - start_time, 3) if not TIMEOUT else 10000
+    if not TIMEOUT:
+        print('Query executed successfully in {} seconds : {}'.format(runtime, query))
     
     return result, runtime
 
@@ -63,32 +64,37 @@ def execute_a_query(query, timeout=500):
 if __name__ == '__main__':
     # 自己把他改成你的query list
     queries = [
-        "SELECT COUNT(*) FROM title t,movie_info mi,movie_info_idx mi_idx,cast_info ci,movie_keyword mk WHERE t.id=mi.movie_id AND t.id=mi_idx.movie_id AND t.id=ci.movie_id AND t.id=mk.movie_id AND mi.info_type_id=3 AND mi_idx.info_type_id=100 AND t.production_year>2010;"
+        "SELECT COUNT(*) FROM movie_companies mc,movie_keyword mk,movie_info_idx mi_idx,title t,movie_info mi WHERE t.id=mi.movie_id AND t.id=mi_idx.movie_id AND t.id=mk.movie_id AND t.id=mc.movie_id AND mk.keyword_id<11604 AND t.kind_id=7 AND t.production_year=2004 AND mi.info_type_id=3;"
     ]
     for query in queries:
-        # original_result, original_time = execute_a_query(query, 1000)
-        # print("The original query:")
-        # print("Time:", original_time)
-        # print("Result:", original_result, '\n')
+        original_result, original_time = execute_a_query(query, 1000)
+        print("The original query:")
+        print("Time:", original_time)
+        print("Result:", original_result, '\n')
 
-        # gspj_query = greedy_selective_pairwise_join(query)
-        # gspj_query_result, gspj_query_time = execute_a_query(gspj_query, original_time*3)
-        # print("The GSPJ query:")
-        # print("Time:", gspj_query_time)
-        # print("Result:", gspj_query_result, '\n')
+        gspj_query = greedy_selective_pairwise_join(query)
+        gspj_query_result, gspj_query_time = execute_a_query(gspj_query, original_time*3)
+        print("The GSPJ query:")
+        print("Time:", gspj_query_time)
+        print("Result:", gspj_query_result, '\n')
 
-        # gldj_query = greedy_left_deep_join(query)
-        # gldj_query_result, gldj_query_time = execute_a_query(gldj_query, original_time*3)
-        # print("The GLDJ query:")
-        # print("Time:", gldj_query_time)
-        # print("Result:", gldj_query_result, '\n')
+        gldj_query = greedy_left_deep_join(query)
+        gldj_query_result, gldj_query_time = execute_a_query(gldj_query, original_time*3)
+        print("The GLDJ query:")
+        print("Time:", gldj_query_time)
+        print("Result:", gldj_query_result, '\n')
 
         all_possible_query = Brutal_Force(query)
-        bf_min_time = 500
+        bf_min_time = original_time + 10
         bf_min_result = None
         bf_query = None
         for i in range(len(all_possible_query)):
             query_result, query_time = execute_a_query(all_possible_query[i], bf_min_time * 2)
+            
+            if(type(query_result) == str): 
+                # Timeout or Error
+                continue
+            
             if(bf_min_result == None):
                 bf_min_result = query_result
                 bf_query = all_possible_query[i]
@@ -101,6 +107,6 @@ if __name__ == '__main__':
         print("Time:", bf_min_time)
         print("Result:", bf_min_result, '\n')
 
-        # with open('output.csv', 'a', newline='') as file:
-        #     writer = csv.writer(file)
-        #     writer.writerow([query.replace('\n', ' ').strip(), original_result, original_time, gspj_query_time, gldj_query_time, bf_min_time, gspj_query.replace('\n', ' ').strip(), gldj_query.replace('\n', ' ').strip(), bf_query.replace('\n', ' ').strip()])
+        with open('output.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([query.replace('\n', ' ').strip(), original_result, original_time, gspj_query_time, gldj_query_time, bf_min_time, gspj_query.replace('\n', ' ').strip(), gldj_query.replace('\n', ' ').strip(), bf_query.replace('\n', ' ').strip()])
